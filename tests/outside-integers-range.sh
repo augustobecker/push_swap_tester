f19e1de79e865e2ff2a9a1133fce2e24387cfb8d
Augusto Becker <81205527+augustobecker@users.noreply.github.com>
2022-08-28 20:07:08 -0300

    Update outside-integers-range.sh

--- tests/outside-integers-range.sh
+++ tests/outside-integers-range.sh
@@ -22,12 +22,12 @@ SOME_NEGATIVES="-10 -3 -5 -99"
       BIG_COMB=$(export ARGS="$(shuf -i 1-100 | xargs)" && echo $ARGS)
 	 HUGE_COMB=$(export ARGS="$(shuf -i 1-500 | xargs)" && echo $ARGS)
 
-      ERROR1=$(./../../$NAME $SMALL_COMB 2147483648 $SOME_NEGATIVES | grep "Error" | wc -l)
-      ERROR2=$(./../../$NAME $SMALL_COMB 002147483648 $SOME_NEGATIVES | grep "Error" | wc -l)
-      ERROR3=$(./../../$NAME $BIG_COMB 2147483658 $SOME_NEGATIVES | grep "Error" | wc -l)
-      ERROR4=$(./../../$NAME $BIG_COMB 002147483658 $SOME_NEGATIVES | grep "Error" | wc -l)
-      ERROR5=$(./../../$NAME $HUGE_COMB -2147483658  $SOME_NEGATIVES | grep "Error" | wc -l)
-      ERROR6=$(./../../$NAME $HUGE_COMB -002147483658 $SOME_NEGATIVES | grep "Error" | wc -l)
+      ERROR1=$(./../$NAME $SMALL_COMB 2147483648 $SOME_NEGATIVES | grep "Error" | wc -l)
+      ERROR2=$(./../$NAME $SMALL_COMB 002147483648 $SOME_NEGATIVES | grep "Error" | wc -l)
+      ERROR3=$(./../$NAME $BIG_COMB 2147483658 $SOME_NEGATIVES | grep "Error" | wc -l)
+      ERROR4=$(./../$NAME $BIG_COMB 002147483658 $SOME_NEGATIVES | grep "Error" | wc -l)
+      ERROR5=$(./../$NAME $HUGE_COMB -2147483658  $SOME_NEGATIVES | grep "Error" | wc -l)
+      ERROR6=$(./../$NAME $HUGE_COMB -002147483658 $SOME_NEGATIVES | grep "Error" | wc -l)
 
 if [ ${ERROR1} -ge 1 ]
 then
