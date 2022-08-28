925f930d99f6a3b1c121ca6b9a205d28813a9ec3
Augusto Becker <81205527+augustobecker@users.noreply.github.com>
2022-08-28 19:07:09 -0300

    Update check-for-repeated-argumet.sh

--- tests/check-for-repeated-argumet.sh
+++ tests/check-for-repeated-argumet.sh
@@ -23,9 +23,12 @@ SOME_NEGATIVES="-10 -3 -5 -99"
       BIG_COMB=$(export ARGS="$(shuf -i 1-100 | xargs)")
 	 HUGE_COMB=$(export ARGS="$(shuf -i 1-500 | xargs)")
 
-      ERROR1=$(./../../$NAME $SMALL_COMB $SOME_NEGATIVES | grep "Error" | wc -l)
-      ERROR2=$(./../../$NAME $BIG_COMB $SOME_NEGATIVES | grep "Error" | wc -l)
-      ERROR3=$(./../../$NAME $HUGE_COMB $SOME_NEGATIVES | grep "Error" | wc -l)
+      ERROR1=$(./../../$NAME $SMALL_COMB $SOME_NEGATIVES  7 | grep "Error" | wc -l)
+      ERROR2=$(./../../$NAME $SMALL_COMB $SOME_NEGATIVES  -99 | grep "Error" | wc -l)
+      ERROR3=$(./../../$NAME $BIG_COMB $SOME_NEGATIVES 22 | grep "Error" | wc -l)
+      ERROR4=$(./../../$NAME $BIG_COMB $SOME_NEGATIVES -3 | grep "Error" | wc -l)
+      ERROR5=$(./../../$NAME $HUGE_COMB $SOME_NEGATIVES 444 | grep "Error" | wc -l)
+      ERROR6=$(./../../$NAME $HUGE_COMB $SOME_NEGATIVES -5 | grep "Error" | wc -l)
 
 if [ ${ERROR1} -ge 1 ]
 then
@@ -42,6 +45,27 @@ else
 fi
 
 if [ ${ERROR3} -ge 1 ]
+then
+	echo -e -n "$GREEN[OK] $RESET"
+else
+	echo -e -n "$RED[KO] $RESET"
+fi
+
+if [ ${ERROR4} -ge 1 ]
+then
+	echo -e -n "$GREEN[OK] $RESET"
+else
+	echo -e -n "$RED[KO] $RESET"
+fi
+
+if [ ${ERROR5} -ge 1 ]
+then
+	echo -e -n "$GREEN[OK] $RESET"
+else
+	echo -e -n "$RED[KO] $RESET"
+fi
+
+if [ ${ERROR6} -ge 1 ]
 then
 	echo -e "$GREEN[OK] $RESET"
 else
